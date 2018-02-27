7e0342d899a8e1543e7e1d5a134988bf2522ab61
SilhDing <dingyihang1994@gmail.com>
2018-02-28 00:47:58 +0800

    finish all function

--- server.js
+++ server.js
@@ -45,14 +45,27 @@ var html_con = function (name, content_toShow) {
             <title>剪贴板应用</title>
         </head>
         
-        <body>   
+        <script type="text/javascript">
+	
+            var info_alert;
+    
+            var save = function () {
+                info_alert = document.getElementById('content').value;
+                alert('您已经保存新的内容：  \\n\\n\\"' + info_alert + '\\"' );
+            }
+        </script>
+    
+        <body>
+ 
             <li><a target="_blank" href="http://www.liaoxuefeng.com/wiki/001434446689867b27157e896e74d51a89c25cc8b43bdb3000">JavaScript教程</a></li>
             <li><a target="_blank" href="http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000">Python教程</a></li>
             <li><a target="_blank" href="https://github.com/SilhDing/hapijs-webapp">应用源码</a></li>   
             <li><a target="_blank" href="https://github.com/hapijs/hapi/issues/3658">Hapi v17 文档参考</a></li>                 
             <h1>Welcome, ${name}!</h1>
-            <textarea name="content" placeholder="请输入新内容" style="min-height: 250px;min-width: 350px;max-height: 250px; max-width: 350px;">${content_toShow}</textarea>
-            <p><button name="save">点击保存</button></p>
+            <form name="form1" method="post" action="http://localhost:9000/">
+	            <p> <textarea type="text" name="textfield" id="content" placeholder="请输入新内容" style="min-height: 250px;min-width: 350px;max-height: 250px; max-width: 350px;">${content_toShow}</textarea></p>
+                <p> <input type="submit" name="button1" value="保存" onclick="void save();"/></p>
+            </form>
         </body>
         </html>
         `;
@@ -73,6 +86,21 @@ var mainPage = async function(request,h) {
     }
 };
 
+var update_fun = async function (new_content) {
+    var contents = await Content.findAll({
+        where: {
+            id: '1'
+        }
+    });
+    for (let p of contents) {
+        p.content = new_content;
+        p.version ++;
+        //console.log(',,,',p)
+        await p.save();
+        return html_con(name,new_content);
+    }
+}
+
 // define route
 const route = {
         method: 'GET',
@@ -84,8 +112,23 @@ const route = {
         }
 };    
 
+const routePost = {
+    method: 'POST',
+    path: '/',
+    options: {
+        handler: (request, h) => {
+            //console.log(typeof(request.payload))
+            //from_user = JSON.parse(request.payload)
+            console.log(request.payload.textfield)
+            return update_fun(request.payload.textfield);
+        }
+    }
+
+}
+
 server.route([
-    route
+    route,
+    routePost
 ]);
 
 async function start() {
